bf12ed7d575f949edaa3c8d5ef16fd5f90629385
worldtvlive231-collab <worldtvlive231@gmail.com>
2026-08-26 21:51:19 +0000

    Fix Stripe checkout launch in mobile browsers

--- stripe-ui-bootstrap.js
+++ stripe-ui-bootstrap.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const express=require("express");
-const STRIPE_SCRIPT='<script src="/assets/stripe-checkout.js?v=20260826-5"></script>';
+const STRIPE_SCRIPT='<script src="/assets/stripe-mobile-redirect.js?v=20260826-1"></script><script src="/assets/stripe-checkout.js?v=20260826-6"></script>';
 const previousStatic=express.static;
 
 express.static=function worldTvStripeUiStatic(root,...args){
