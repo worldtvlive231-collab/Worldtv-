e321d77f2d30683ef6385343e660c11a5e52fd7a
worldtvlive231-collab <worldtvlive231@gmail.com>
2026-08-26 22:07:55 +0000

    Fix Stripe scripts on subscription checkout page

--- stripe-ui-bootstrap.js
+++ stripe-ui-bootstrap.js
@@ -1,28 +1,34 @@
 "use strict";
 
 const express=require("express");
+const fs=require("fs");
+const path=require("path");
 const STRIPE_SCRIPT='<script src="/assets/stripe-mobile-redirect.js?v=20260826-1"></script><script src="/assets/stripe-checkout.js?v=20260826-6"></script>';
 const previousStatic=express.static;
+
+function injectStripeScripts(html){
+  let output=String(html||"");
+  const stripeTag=/<script\s+src=["']\/assets\/stripe-checkout\.js(?:\?[^"']*)?["']><\/script>/i;
+  if(stripeTag.test(output)) output=output.replace(stripeTag,STRIPE_SCRIPT);
+  else output=output.includes("</body>")?output.replace("</body>",`${STRIPE_SCRIPT}\n</body>`):output+STRIPE_SCRIPT;
+  return output;
+}
 
 express.static=function worldTvStripeUiStatic(root,...args){
   const middleware=previousStatic(root,...args);
   return function(req,res,next){
     if(String(req.path||"").toLowerCase()!=="/subscribe.html")return middleware(req,res,next);
-    const originalSend=res.send;
-    res.send=function worldTvStripeInjectedSend(body){
-      res.send=originalSend;
-      let output=body;
+
+    const filePath=path.join(root,"subscribe.html");
+    fs.readFile(filePath,"utf8",(err,html)=>{
+      if(err)return middleware(req,res,next);
       try{
-        if(typeof output==="string"){
-          const stripeTag=/<script\s+src=["']\/assets\/stripe-checkout\.js(?:\?[^"']*)?["']><\/script>/i;
-          if(stripeTag.test(output)) output=output.replace(stripeTag,STRIPE_SCRIPT);
-          else output=output.includes("</body>")?output.replace("</body>",`${STRIPE_SCRIPT}\n</body>`):output+STRIPE_SCRIPT;
-        }
         res.setHeader("Cache-Control","no-cache, no-store, must-revalidate");
-      }catch(_){ }
-      return originalSend.call(res,output);
-    };
-    return middleware(req,res,(err)=>{res.send=originalSend;next(err);});
+        res.type("html").send(injectStripeScripts(html));
+      }catch(_){
+        return middleware(req,res,next);
+      }
+    });
   };
 };
 
